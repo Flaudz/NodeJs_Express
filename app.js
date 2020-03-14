873acd2c840be669e0f135d2dcc2b78e5ae88123
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 21:35:32 +0100

    Added contact-success.ejs in views added a contact form

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
-
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.listen(5050);
 
 app.set("view engine", "ejs");
@@ -12,8 +12,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-	console.log(req.query);
 	res.render(`contact`, { qs: req.query });
+});
+
+app.post("/contact", urlencodedParser, (req, res) => {
+	console.log(req.body);
+	res.render(`contact-success`, { data: req.body });
 });
 
 app.get("/profile/:id", (req, res) => {
