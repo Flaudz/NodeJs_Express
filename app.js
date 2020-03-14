6f618a1fd7a49e6c8c989295e54654850c636c67
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 17:32:03 +0100

    Added Route params

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.get("/", (req, res) => {
 app.get("/contact", (req, res) => {
 	res.send("This is the contact page");
 });
+
+app.get("/profile/:id", (req, res) => {
+	res.send(`You requested to see a profile with the if of ${req.params.id}`);
+});
